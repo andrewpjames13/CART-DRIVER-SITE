78cfe83130615ebb3e59c597750087a125af16e1
Andrew James <andrewpjames87@gmail.com>
2017-02-06 21:32:59 -0700

    Home background image fix for mobile

--- src/components/home_screen.js
+++ src/components/home_screen.js
@@ -3,8 +3,15 @@ import React from 'react';
 import CountDown from './count_down';
 
 const HomeScreen = (props) => {
+  let backgroundImage;
+  if (window.innerWidth >= 768) {
+    backgroundImage = 'url(images/cart-driver-oven-pizza.jpg)';
+  } else {
+    backgroundImage = 'url(images/cart-driver-oven-pizza.jpg)';
+  }
+
   return (
-    <div className="home-container" style={{'backgroundImage': 'url(images/cart-driver-oven-pizza.jpg)'}}>
+    <div className="home-container" style={{'backgroundImage': backgroundImage}}>
       <div className="tiny-100 home-content">
         <h1 className="bold home-title">CART-DRIVER</h1>
         <CountDown />
