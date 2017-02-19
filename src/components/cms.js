76c5248d506a35ac0ba84efcd1728a7ebdac7502
Andrew James <andrewpjames87@gmail.com>
2017-02-18 18:05:45 -0700

    Add react router with a home and cms route

---
+++ src/components/cms.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+import React, { Component } from 'react';
+
+class Cms extends Component {
+  render() {
+    return (
+      <div>Cms</div>
+    );
+  }
+}
+
+export default Cms;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 /*jshint esversion: 6 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './App';
+import Cms from './components/cms';
 // Stylesheets
 import './style/App.scss';
 
@@ -17,7 +19,10 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+      <Route path="/cms" component={Cms} />
+    </Router>
   </Provider>
   , document.getElementById('root')
 );
