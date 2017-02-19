be49a101f0c0d640286d33b3ced56d99086e13be
Andrew James <andrewpjames87@gmail.com>
2017-02-18 17:12:47 -0700

    Pull data from firebase db

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,25 @@
 /*jshint esversion: 6 */
-import axios from 'axios';
+import * as firebase from 'firebase';
 import { FETCH_DATA_SUCCESS } from '../constants/ActionTypes';
 
-function fetchDataSuccess(data) {
-  return {
-    type: FETCH_DATA_SUCCESS,
-    payload: data
-  };
-}
+firebase.initializeApp({
+  apiKey: "AIzaSyDQqxKxGV_0cxpi0AxStdMJ2xQV0wBRMhk",
+  authDomain: "cart-driver.firebaseapp.com",
+  databaseURL: "https://cart-driver.firebaseio.com",
+  storageBucket: "cart-driver.appspot.com",
+  messagingSenderId: "781002909952"
+});
+
+const Data = firebase.database().ref();
 
 export function fetchData() {
   return dispatch => {
-    axios.get('/data.json')
-      .then((response) => {
-        dispatch(fetchDataSuccess(response.data));
+    Data.on('value', snapshot => {
+      console.log(snapshot.val(), 'snapshot');
+      dispatch({
+        type: FETCH_DATA_SUCCESS,
+        payload: snapshot.val()
       });
+    });
   };
 }
