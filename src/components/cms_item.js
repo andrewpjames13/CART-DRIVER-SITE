18c54e02e4635155b337f0042999d095ec19f128
Andrew James <andrewpjames87@gmail.com>
2017-03-19 12:53:13 -0600

    Add form to cms item with open animation

--- src/components/cms_item.js
+++ src/components/cms_item.js
@@ -1,12 +1,15 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
+import CmsForm from './cms_form';
+import classNames from 'classnames';
 
 class CmsItem extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      animate: '0'
+      animate: '0',
+      openUpdate: false
     };
   }
 
@@ -16,33 +19,49 @@ class CmsItem extends Component {
     });
   }
 
+  openUpdateItem() {
+    this.setState({
+      openUpdate: !this.state.openUpdate
+    });
+  }
+
   render() {
+    const styles = classNames({
+      "form-container": true,
+      open: this.state.openUpdate
+    });
+
     return (
-      <div className="cms-item" style={{transform: `translateX(${this.state.animate})`}}>
-        <h2 className="bold" key={this.props.item.name}>
-          {this.props.item.name}
-        </h2>
-        <div className="more-menu">
-          <button className="cta more" onClick={() => this.animateMenu()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M12 18c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3z"/>
-            </svg>
-          </button>
-          <button className="cta edit" onClick={() => this.animateMenu()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/>
-            </svg>
-          </button>
-          <button className="cta delete" onClick={() => this.props.deleteMenuItem(this.props.selectedMenu, this.props.index)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M9 19c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5-17v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712zm-3 4v16h-14v-16h-2v18h18v-18h-2z"/>
-            </svg>
-          </button>
-          <button className="cta move" onClick={() => this.animateMenu()}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M14 12c0 1.104-.896 2-2 2s-2-.896-2-2 .896-2 2-2 2 .896 2 2zm-3-3.858c.321-.083.653-.142 1-.142s.679.059 1 .142v-2.142h4l-5-6-5 6h4v2.142zm2 7.716c-.321.083-.653.142-1 .142s-.679-.059-1-.142v2.142h-4l5 6 5-6h-4v-2.142z"/>
-            </svg>
-          </button>
+      <div>
+        <div className="cms-item" style={{transform: `translateX(${this.state.animate})`}}>
+          <h2 className="bold" key={this.props.item.name}>
+            {this.props.item.name}
+          </h2>
+          <div className="more-menu">
+            <button className="cta more" onClick={() => this.animateMenu()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M12 18c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3z"/>
+              </svg>
+            </button>
+            <button className="cta edit" onClick={() => this.openUpdateItem()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/>
+              </svg>
+            </button>
+            <button className="cta delete" onClick={() => this.props.deleteMenuItem(this.props.selectedMenu, this.props.index)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M9 19c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5-17v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712zm-3 4v16h-14v-16h-2v18h18v-18h-2z"/>
+              </svg>
+            </button>
+            <button className="cta move" onClick={() => this.animateMenu()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M14 12c0 1.104-.896 2-2 2s-2-.896-2-2 .896-2 2-2 2 .896 2 2zm-3-3.858c.321-.083.653-.142 1-.142s.679.059 1 .142v-2.142h4l-5-6-5 6h4v2.142zm2 7.716c-.321.083-.653.142-1 .142s-.679-.059-1-.142v2.142h-4l5 6 5-6h-4v-2.142z"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={styles}>
+          <CmsForm />
         </div>
       </div>
     );
