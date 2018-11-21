dd559742f970df30626fc611e6dade521aa4c1f2
andrewpjames13 <andrew.james@acumendigital.com>
2018-11-21 07:41:31 -0700

    Update hero image

--- src/components/home_screen.js
+++ src/components/home_screen.js
@@ -1,18 +1,10 @@
-/*jshint esversion: 6 */
 import React from 'react';
 import CountDown from './count_down';
 
-const HomeScreen = (props) => {
-  let backgroundImage;
-  if (window.innerWidth >= 768) {
-    backgroundImage = 'url(images/cart-driver-oven-pizza.jpg)';
-  } else {
-    backgroundImage = '';
-  }
-
+const HomeScreen = () => {
   return (
-    <div className="home-container" style={{"backgroundImage" : backgroundImage}}>
-      <img className="home-img" src="images/cart-driver-oven-pizza-mobile.jpg" role="presentation"/>
+    <div className="home-container" style={{ backgroundImage: 'url(images/cart-driver-table-party.jpg)'}}>
+      <div className="home-radial-grad" />
       <div className="tiny-100 home-content">
         <CountDown />
         <h6 className="bold open-times">OPEN 12PM - 12AM SEVEN DAYS A WEEK</h6>
